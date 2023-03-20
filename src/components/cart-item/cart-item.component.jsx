1172e12d2898face0a5e7e2cb986c0ebd34c0395
Usama Bin Tariq <ut303397@gmail.com>
2023-03-20 14:38:49 +0500

    first commit

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,16 @@
+import {CartItemContainer, Image, ItemDetails, Name} from "./cart-item.styles.jsx";
+
+const CartItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <CartItemContainer>
+      <Image src={imageUrl} alt={`${name}`} />
+      <ItemDetails>
+        <Name className="name">{name}</Name>
+      <span className="price">{quantity} x ${price}</span>
+      </ItemDetails>
+    </CartItemContainer>
+  );
+};
+
+export default CartItem;
